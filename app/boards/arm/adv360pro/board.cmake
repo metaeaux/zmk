75e1c34d5c902b5168d89c8742da04553eacc8d2
urob <978080+urob@users.noreply.github.com>
2023-10-03 14:56:17 -0400

    Adv360: official PR

--- app/boards/arm/adv360pro/board.cmake
+++ app/boards/arm/adv360pro/board.cmake
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #
 # Copyright (c) 2023 The ZMK Contributors
 # SPDX-License-Identifier: MIT
@@ -6,3 +7,15 @@
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+||||||| parent of a82932f0 (Adv360: official PR)
+=======
+#
+# Copyright (c) 2022 The ZMK Contributors
+# SPDX-License-Identifier: MIT
+#
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+>>>>>>> a82932f0 (Adv360: official PR)
